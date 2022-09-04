e152eebee6262fbdfb1dcbed591062015655a51e
Dimonchik <sudjadred@live.com>
2022-09-04 23:40:23 +0200

    3 решения 9 задачи

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -5,17 +5,31 @@
 // 12-> 2
 // 85 -> 8
 
-int num = new Random().Next(10, 100);
-int firdig = num / 10;
-int secdig = num % 10;
+int number = new Random().Next(10, 100);
+// int firdig = num / 10;
+// int secdig = num % 10;
 
 //if (firdig > secdig) Console.WriteLine($"Большая цифра из числа {num} -> {firdig}");
 //else Console.WriteLine($"Большая цифра {num} -> {secdig}");
 
+
 // тернарный опратор
-if (firdig == secdig) Console.WriteLine($"{num} Одинаковые числа");
-else
+// if (firdig == secdig) Console.WriteLine($"{num} Одинаковые числа");
+// else
+// {
+// int max = firdig > secdig ? firdig : secdig;
+// Console.WriteLine($"Большая цифра из числа {num} -> {max}");
+// }
+
+int MaxDieg(int num)
+
 {
-int max = firdig > secdig ? firdig : secdig;
-Console.WriteLine($"Большая цифра из числа {num} -> {max}");
+    int firdig = number / 10;
+    int secdig = number % 10;
+    if (firdig == secdig) return 0;
+    return firdig > secdig ? firdig : secdig;
 }
+
+int maxDieg = MaxDieg(number);
+string result = maxDieg > 0 ? maxDieg.ToString() : "Числа равны";
+Console.WriteLine($"Большая цифра из числа {number} -> {result}");
