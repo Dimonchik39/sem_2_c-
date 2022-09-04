00f55a4a6c4fd94569308c45ddbcce69ce8f774a
Dimonchik <sudjadred@live.com>
2022-09-04 21:36:24 +0200

    решили задачу 9 стандартно

---
+++ Task09/Program.cs
@@ -0,0 +1,21 @@
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int num = new Random().Next(10, 100);
+int firdig = num / 10;
+int secdig = num % 10;
+
+//if (firdig > secdig) Console.WriteLine($"Большая цифра из числа {num} -> {firdig}");
+//else Console.WriteLine($"Большая цифра {num} -> {secdig}");
+
+// тернарный опратор
+if (firdig == secdig) Console.WriteLine($"{num} Одинаковые числа");
+else
+{
+int max = firdig > secdig ? firdig : secdig;
+Console.WriteLine($"Большая цифра из числа {num} -> {max}");
+}
